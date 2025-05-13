ad3fae9a1906122c65cdd9943081a628ec7999e7
tox-jia <oeelo666@gmail.com>
2025-05-14 00:23:13 +0800

    added allowed hosts for railway

--- ecom2/settings.py
+++ ecom2/settings.py
@@ -34,8 +34,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cart',
     'store',
+    'cart',
     'payment',
     'whitenoise.runserver_nostatic',
 ]
