35b13f5c94949fd970143b19e81644a3528f6334
tox-jia <oeelo666@gmail.com>
2025-05-14 22:05:20 +0800

    Remove MEDIA_ROOT reference for Cloudinary

--- ecom2/urls.py
+++ ecom2/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', include('store.urls')),
     path('cart/', include('cart.urls')),
     path('payment/', include('payment.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
