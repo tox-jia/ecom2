bfe9f57ca77daf56b07d08e921c8bf84ecf5fa55
tox-jia <oeelo666@gmail.com>
2025-05-14 22:23:53 +0800

    update

--- ecom2/settings.py
+++ ecom2/settings.py
@@ -139,9 +139,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = ['static/']
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+    'CLOUD_NAME': os.environ['CLOUDINARY_CLOUD_NAME'],
+    'API_KEY': os.environ['CLOUDINARY_API_KEY'],
+    'API_SECRET': os.environ['CLOUDINARY_API_SECRET'],
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
