c93f94d2709eaada8155b0e81fde7ce6e20387dd
tox-jia <oeelo666@gmail.com>
2025-06-23 22:28:18 +0800

    checkout & del_record in checkout.html

--- time_sys/views.py
+++ time_sys/views.py
@@ -19,6 +19,7 @@ from .utils import timezone_display, month_jump
 @user_passes_test(lambda u: u.is_superuser)
 def time_checkout(request):
     form = TimeCheckoutForm(request.POST or None)
+    form_del = RecordDel(request.POST or None)
     tags = TimeTag.objects.all()
     record_qs = TimeRecord.objects.all()
     checkout_time = timezone.now()
@@ -27,69 +28,7 @@ def time_checkout(request):
         messages.error(request, 'Please create a Time-Tag first.')
         return redirect('time_settings')
 
-    if record_qs.exists():
-        this_utc = timezone.now()
-        last_utc = record_qs.order_by('-id').first().end
-
-        if request.method == 'POST' and form.is_valid():
-            time_switch = form.cleaned_data['time_switch']
-
-            if time_switch:
-                this_raw = form.cleaned_data['time_correction']
-                if timezone.is_aware(this_raw):
-                    this_naive = this_raw.replace(tzinfo=None)
-                    #// strip off the timezone info by using replace(tzinfo=None)
-                else:
-                    this_naive = this_raw
-
-                user_tz = pytz.timezone(request.user.profile.timezone)
-                this_local = user_tz.localize(this_naive)
-
-                this_utc = this_local.astimezone(pytz.UTC)
-
-                if this_utc <= last_utc or this_utc > timezone.now():
-                    messages.error(request, "Invalid time.")
-                    return redirect('time_checkout')
-
-                duration = this_utc - last_utc
-                checkout_time = this_utc
-            else:
-                duration = this_utc - last_utc
-                checkout_time = this_utc
-
-            tag_raw = form.cleaned_data['time_tag'].split("-", 2)
-            # ----------------------------------#
-            # Month Jump #
-            # // by using "utlis.py"
-            # ------------ Start ---------------#
-            month_jump_result = month_jump(last_utc, this_utc)
-            if month_jump_result:
-                TimeRecord.objects.create(
-                    tag=tag_raw[0],
-                    end=month_jump_result['end1'],
-                    duration=month_jump_result['duration1'],
-                    type=tag_raw[1]
-                )
-                TimeRecord.objects.create(
-                    tag=tag_raw[0],
-                    end=checkout_time,
-                    duration=month_jump_result['duration2'],
-                    type=tag_raw[1]
-                )
-            else:
-                TimeRecord.objects.create(
-                    tag=tag_raw[0],
-                    end=checkout_time,
-                    duration=int(duration.total_seconds()),
-                    type=tag_raw[1]
-                )
-                # ----------------------------------#
-                # End #
-                # ------------ Start ---------------#
-            # messages.success(request, "Time checkout successfully.")
-            return redirect('time_checkout')
-
-    else:
+    if not record_qs.exists():
         TimeRecord.objects.create(
             tag='start',
             end=checkout_time,  # <- this_utc, not datetime.this_utc()
@@ -99,15 +38,102 @@ def time_checkout(request):
         messages.success(request, "Started first record.")
         return redirect('time_checkout')
 
+    else:
+        this_utc = timezone.now()
+        last_utc = record_qs.order_by('-id').first().end
+
+        if request.method == 'POST':
+            form_type = request.POST.get("form_type")
+            # ----------------------------------#
+            # Checkout #
+            # ------------ Start ---------------#
+            if form_type=="checkout" and form.is_valid():
+                tag_raw = form.cleaned_data['time_tag'].split("-", 2)
+                time_switch = form.cleaned_data['time_switch']
+                # ----------------------------------#
+                # Switch is on #
+                # ------------ Start ---------------#
+                if time_switch:
+                    this_raw = form.cleaned_data['time_correction']
+                    if timezone.is_aware(this_raw):
+                        this_naive = this_raw.replace(tzinfo=None)
+                        # // strip off the timezone info by using replace(tzinfo=None)
+                    else:
+                        this_naive = this_raw
+
+                    user_tz = pytz.timezone(request.user.profile.timezone)
+                    this_local = user_tz.localize(this_naive)
+
+                    this_utc = this_local.astimezone(pytz.UTC)
+
+                    if this_utc <= last_utc or this_utc > timezone.now():
+                        messages.error(request, "Invalid time.")
+                        return redirect('time_checkout')
+
+                    duration = this_utc - last_utc
+                    checkout_time = this_utc
+                # ----------------------------------#
+                # Switch is off #
+                # ------------ Start ---------------#
+                else:
+                    duration = this_utc - last_utc
+                    checkout_time = this_utc
+                # END ------------------------------#
+
+
+                # ----------------------------------#
+                # Month Jump #
+                # // by using "utlis.py"
+                # ------------ Start ---------------#
+                month_jump_result = month_jump(last_utc, this_utc)
+                if month_jump_result:
+                    TimeRecord.objects.create(
+                        tag=tag_raw[0],
+                        end=month_jump_result['end1'],
+                        duration=month_jump_result['duration1'],
+                        type=tag_raw[1]
+                    )
+                    TimeRecord.objects.create(
+                        tag=tag_raw[0],
+                        end=checkout_time,
+                        duration=month_jump_result['duration2'],
+                        type=tag_raw[1]
+                    )
+                else:
+                    TimeRecord.objects.create(
+                        tag=tag_raw[0],
+                        end=checkout_time,
+                        duration=int(duration.total_seconds()),
+                        type=tag_raw[1]
+                    )
+                # END ------------------------------#
+
+
+                # messages.success(request, "Time checkout successfully.")
+                return redirect('time_checkout')
+            # END ------------------------------#
+
+
+            # ----------------------------------#
+            # Delete the record by clicking 'x' #
+            # ----------------------------------#
+            elif form_type=="del" and form_del.is_valid():
+                record_del = TimeRecord.objects.filter(id=form_del.cleaned_data['del_id']).first()
+                record_del.delete()
+                return redirect('time_checkout')
+            # END ------------------------------#
+
+
     if form.errors:
         for field, errs in form.errors.items():
             for err in errs:
                 messages.error(request, f'{field}: {err}')
 
-    records = TimeRecord.objects.order_by('-id')[:1]
+    records = TimeRecord.objects.order_by('-id')[:2]
 
     context = {
         'form': form,
+        'form_del':form_del,
         'tags': tags,
         'timezone': timezone_display(request.user.profile),
         'now': this_utc,
